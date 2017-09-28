3fd83274010139a5746302f6f41f6c705570bf67
Kaiichiro Ota <kaiichiro@edward.local>
2017-09-29 00:45:52 +0900

    add PouchDB

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,5 @@
+import PouchDB from 'pouchdb'
+
 const noteList = document.getElementById('note-list');
 
 for (let i = 1; i <= 100; i++) {
@@ -5,3 +7,18 @@ for (let i = 1; i <= 100; i++) {
     item.innerText = `Note ${i}`;
     noteList.appendChild(item);
 }
+
+const db = new PouchDB('http://localhost:5984/test');
+
+const doc = {
+    '_id': "あいうえお",
+    "name": "Mittens",
+    "occupation": "kitten",
+    "age": 3,
+    "hobbies": [
+        "playing with balls of yarn",
+        "chasing laser pointers",
+        "lookin' hella cute"
+    ]
+};
+db.put(doc);
