af10665c0e9675b24b2c334f4bbd787eabcea11b
Kaiichiro Ota <kaiichiro@edward.local>
2017-09-29 23:39:20 +0900

    fix eslint errors

--- src/renderer.js
+++ src/renderer.js
@@ -1,24 +1,19 @@
-import PouchDB from 'pouchdb'
+import PouchDB from 'pouchdb';
 
 const noteList = document.getElementById('note-list');
 
-for (let i = 1; i <= 100; i++) {
-    const item = document.createElement('ul');
-    item.innerText = `Note ${i}`;
-    noteList.appendChild(item);
+for (let i = 1; i <= 100; i += 1) {
+  const item = document.createElement('ul');
+  item.innerText = `Note ${i}`;
+  noteList.appendChild(item);
 }
 
 const db = new PouchDB('http://localhost:5984/test');
 
 const doc = {
-    '_id': "あいうえお",
-    "name": "Mittens",
-    "occupation": "kitten",
-    "age": 3,
-    "hobbies": [
-        "playing with balls of yarn",
-        "chasing laser pointers",
-        "lookin' hella cute"
-    ]
+  _id: 'あいうえお',
+  name: 'Mittens',
+  occupation: 'kitten',
+  age: 3,
 };
 db.put(doc);
