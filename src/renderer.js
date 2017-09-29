9f627a49ce9dda0d3121d8e9fa68ab2380e11b4b
kigh-ota <kigh.ota@gmail.com>
2017-09-30 00:55:04 +0900

    can add & update a note

--- src/renderer.js
+++ src/renderer.js
@@ -10,10 +10,43 @@ for (let i = 1; i <= 100; i += 1) {
 
 const db = new PouchDB('http://localhost:5984/test');
 
-const doc = {
-  _id: 'あいうえお',
-  name: 'Mittens',
-  occupation: 'kitten',
-  age: 3,
-};
-db.put(doc);
+/**
+ * @param {string} title
+ * @param {string} body
+ * @returns {Promise<any>}
+ */
+function addNote(title, body) {
+  return db.get(title)
+    .then(() => Promise.reject('Note already exists'))
+    .catch(() => {
+      const dt = new Date().toISOString();
+      return db.put({
+        _id: title,
+        title,
+        body,
+        createdAt: dt,
+        updatedAt: dt,
+      });
+    });
+}
+
+/**
+ * @param {string} title
+ * @param {string} body
+ * @returns {Promise<any>}
+ */
+function updateNote(title, body) {
+  return db.get(title)
+    .then(doc => db.put({
+      _id: title,
+      _rev: doc._rev,
+      title,
+      body,
+      createdAt: doc.createdAt,
+      updatedAt: new Date().toISOString(),
+    }))
+    .catch(() => Promise.reject('Note not found'));
+}
+
+addNote('TITLE', 'BODY');
+updateNote('TITLE', 'BODY');
