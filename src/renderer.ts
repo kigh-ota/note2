4fc19f43e075904933532f62fe5c9784f184a4de
kigh-ota <kigh.ota@gmail.com>
2017-09-30 20:26:19 +0900

    refactor

--- src/renderer.ts
+++ src/renderer.ts
@@ -7,18 +7,25 @@ const bodyInput: HTMLTextAreaElement = document.getElementById('note-body-input'
 const noteList: HTMLUListElement = document.querySelector('#note-list > ul') as HTMLUListElement;
 const statusBar: HTMLElement = document.getElementById('status-bar-inner') as HTMLElement;
 
-repository.getAll().then(notes => {
-  notes.forEach((note: any) => {
-    const item: HTMLLIElement = document.createElement('li') as HTMLLIElement;
-    item.innerText = note.title;
-    item.addEventListener('click', () => {
-      titleInput.value = note.title;
-      bodyInput.textContent = note.body;
-      statusBar.innerText = `id: ${note._id}`;
+function clearNoteList(): void {
+  noteList.innerHTML = '';
+}
+
+function refreshNoteList(): Promise<any> {
+  clearNoteList();
+  return repository.getAll().then(notes => {
+    notes.forEach((note: any) => {
+      const item: HTMLLIElement = document.createElement('li') as HTMLLIElement;
+      item.innerText = note.title;
+      item.addEventListener('click', () => {
+        titleInput.value = note.title;
+        bodyInput.textContent = note.body;
+        setStatusBar(`id: ${note._id}`);
+      });
+      noteList.appendChild(item);
     });
-    noteList.appendChild(item);
   });
-});
+}
 
 function getTitle(): string {
   return titleInput.value;
@@ -28,11 +35,18 @@ function getBody(): string {
   return bodyInput.textContent as string;
 }
 
-statusBar.innerText = 'This is status bar';
+function setStatusBar(text: string): void {
+  statusBar.innerText = text;
+}
 
-document.addEventListener('keydown', (e: any) => {
-  console.log(e);
-  if (e.ctrlKey && (e.key === 's' || e.key === 'S')) {
-    repository.add(getTitle(), getBody());
-  }
-});
+function registerKeyEventHandler(): void {
+  document.addEventListener('keydown', (e: KeyboardEvent) => {
+    console.log(e);
+    if (e.ctrlKey && (e.key === 's' || e.key === 'S')) {
+      repository.add(getTitle(), getBody());
+    }
+  });
+}
+
+registerKeyEventHandler();
+refreshNoteList();
