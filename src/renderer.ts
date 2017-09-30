ae6a80947a894ff2525b5e551602e823909369b2
kigh-ota <kigh.ota@gmail.com>
2017-09-30 20:52:21 +0900

    highlight opened note

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,5 @@
 import NoteRepository from './NoteRepository';
+import * as $ from 'jquery';
 
 const repository: NoteRepository = new NoteRepository();
 
@@ -11,19 +12,27 @@ function clearNoteList(): void {
   noteList.innerHTML = '';
 }
 
+let openedNoteId: string|null = null;
+
 function refreshNoteList(): Promise<any> {
   clearNoteList();
   return repository.getAll().then(notes => {
-    notes.forEach((note: any) => {
-      const item: HTMLLIElement = document.createElement('li') as HTMLLIElement;
-      item.innerText = note.title;
-      item.addEventListener('click', () => {
-        titleInput.value = note.title;
-        bodyInput.textContent = note.body;
-        setStatusBar(`id: ${note._id}`);
+    notes
+      .sort((a: any, b: any) => a.updatedAt > b.updatedAt ? -1 : 1)
+      .forEach((note: any) => {
+        const item: HTMLLIElement = document.createElement('li') as HTMLLIElement;
+        item.innerText = note.title;
+        item.dataset.id = note._id;
+        item.addEventListener('click', () => {
+          titleInput.value = note.title;
+          bodyInput.textContent = note.body;
+          openedNoteId = note._id;
+          $(noteList).find('li').removeClass('opened-note');
+          item.classList.add('opened-note');
+          setStatusBar(`id: ${note._id}`);
+        });
+        noteList.appendChild(item);
       });
-      noteList.appendChild(item);
-    });
   });
 }
 
